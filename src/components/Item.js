353fb55ab22efe7acef375acbf1c294d04727a54
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-22 12:34:26 -0600

    Developed useState Solutions for Lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+// PseudoCode
+
+// Needs to be an event handler onClick
+// pass {function} through this handler that:
+    // checks an if statement
+      // changes string assigned to className to "in-cart" if useState = true
+      // ELSE useState = False and variable assigned to string assigned to className is ""
+
+  const [inCart, setCartStatus] = useState(false)     
+  const addToCart = () => {
+    return setCartStatus(!inCart);
+}
+
   return (
-    <li className="">
+
+    <li className = {inCart ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {addToCart}>{inCart ? 'Remove From Cart' : 'Add to Cart'}</button>
     </li>
   );
 }
